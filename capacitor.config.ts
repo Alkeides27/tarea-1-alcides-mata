4919ccb654f0bd3db2d0e5fff492344d966ade74
Alkeides27 <alkeidesantonio@gmail.com>
2026-04-22 20:11:18 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea 1 - Alcides Mata',
+  webDir: 'www'
+};
+
+export default config;
